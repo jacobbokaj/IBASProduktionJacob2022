f934eb3e3fa5060491115320e936637859d150ad
jacobbokaj <44619120+jacobbokaj@users.noreply.github.com>
2022-11-07 13:22:32 +0100

    Update DailyProductionController.cs

--- Controllers/DailyProductionController.cs
+++ Controllers/DailyProductionController.cs
@@ -24,7 +24,7 @@ namespace IbasAPI.Controllers
             _logger = logger;
 
             //this.tableClient = new TableClient("DefaultEndpointsProtocol = https; AccountName = jacob360; AccountKey = LK7AJjQBghV7mJPDR7XGD0w6Phc5tcssfaSQA3B + u / T5tDQKN7JsF4ClIX6xwBWIwobH + p5iy15H + AStOgeCKQ ==; BlobEndpoint = https://jacob360.blob.core.windows.net/;QueueEndpoint=https://jacob360.queue.core.windows.net/;TableEndpoint=https://jacob360.table.core.windows.net/;FileEndpoint=https://jacob360.file.core.windows.net/;");
-            this.tableClient = new TableClient(new Uri("https://jacob360.table.core.windows.net"), "IBASProduktion2022", new TableSharedKeyCredential("jacob360", "LK7AJjQBghV7mJPDR7XGD0w6Phc5tcssfaSQA3B+u/T5tDQKN7JsF4ClIX6xwBWIwobH+p5iy15H+AStOgeCKQ=="));
+            this.tableClient = new TableClient(new Uri("https://jacob360.table.core.windows.net"), "IBASProduktion2022Jacob", new TableSharedKeyCredential("jacob360", "ksz2+P/Ljz5IaWK/tSZX8TROrciUFnipECk1SWJ1hr1scY+z6acHYoRZu7zTgBd6on72huQkkp5I+ASt1h4mOw=="));
             Pageable <TableEntity> entities = this.tableClient.Query<TableEntity>();
 
         }
